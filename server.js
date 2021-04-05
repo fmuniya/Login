06302d474db36206181a4ee791d632adb33525f2
fmuniya <fmuniya@gmail.com>
2021-04-05 21:36:07 +0200

    fixed output issue on array in terminal

--- server.js
+++ server.js
@@ -25,11 +25,11 @@ app.get('/register', (req, res) => {
 
 app.post('/register', async (req, res) => {
     try{
-     const hashedPasword = await bcrypt.hash(req.body.passord, 10)  
+     const hashedPasword = await bcrypt.hash(req.body.pass, 10)  
         users.push({
             id: Date.now().toString(),
             name: req.body.name,
-            email: req.body.email,
+            email: req.body.mail,
             password: hashedPasword
         })
         res.redirect('/login')
