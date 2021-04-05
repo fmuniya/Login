4e6bf020296420fc75eb7902bb7a332eb954822d
fmuniya <fmuniya@gmail.com>
2021-04-05 21:17:27 +0200

    no output in terminal array for registered users

--- server.js
+++ server.js
@@ -26,11 +26,19 @@ app.get('/register', (req, res) => {
 app.post('/register', async (req, res) => {
     try{
      const hashedPasword = await bcrypt.hash(req.body.passord, 10)  
-
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPasword
+        })
+        res.redirect('/login')
     }catch{
+        res.redirect('/register')
 
     }
-    req.body.email  
+    console.log(users)
+    
 })
 
 app.listen(3000)
